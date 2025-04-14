3fde570d3dba56bbf60bcf1df28e4a225c2ded68
Ojasva Goyal <97733600+Ojasva-Goyal@users.noreply.github.com>
2025-04-14 09:44:16 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,9 @@ import { HashRouter } from 'react-router-dom'; // Import HashRouter
 import App from './App.tsx';
 import './index.css';
 
-// createRoot(document.getElementById("root")!).render(<App />);
-createRoot(document.getElementById("root")!).render(
-    <HashRouter>
-      <App />
-    </HashRouter>
-  );
+createRoot(document.getElementById("root")!).render(<App />);
+// createRoot(document.getElementById("root")!).render(
+//     <HashRouter>
+//       <App />
+//     </HashRouter>
+//   );
